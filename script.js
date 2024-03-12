735f5d08f364900fcc87095748f52c11e619e50d
Aastha Gupta <itsaastha.2005@gmail.com>
2024-03-13 01:03:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const scroller = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container"),
+    smooth: true,
+});
+
+gsap.registerPlugin(Flip);
+CustomEase.create("cubic","0.83 , 0 , 0.17 ,1");
+
+const gallery = document.querySelector(".img-gal-cont");
+const images = gsap.utils.toArray(".img");
+let rotationValues= [10,-5,2,-2,0.5];
+
+let isFlipped = false;
+
+function applyRotation(){
+    images.forEach((img,index) => {
+        const rotation = isFlipped ? 0 : rotationValues[index];
+        gsap.to(img , {
+            rotate: rotation,
+            duration: 2,
+            ease: "cubic",
+            delay: 0.155,
+        });
+    });
+}
+
+document.querySelector(".btn").addEventListener("click", ()=>{
+    isFlipped= !isFlipped;
+
+    setTimeout(()=>{
+        document.querySelector(".btn").textContent= isFlipped ? "Hide All Ideas"
+        : "Explores Ideas";
+    },1000);
+
+    let state=Flip.getState(".img-gal-cont, .img");
+    gallery.classList.toggle("order");
+    images.forEach((img) => img.classList.toggle("reorder"));
+
+    Flip.from(state, {
+        absolute: true,
+        duration: 2,
+        rotate: 0,
+        stagger: 0.05,
+        ease: "cubic",
+        onStart: () => {
+            applyRotation();
+        },
+        onComplete: () => {
+            scroller.update();
+        },
+    })
+})
